bc0afbb4857463a717109a4da2154f393462e624
Raven <you@example.com>
2021-06-03 19:50:18 +0800

    generate README.md

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,35 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title} 
+  
+  ## Description
+  ${data.description}
 
+  ## Installation
+  ${data.installation}
+
+  ## License
+  Protected by MIT License 
+
+  ## Usage
+  ${data.demo}
+  
+  ## Credit
+  > ${data.contributorName}
+  > ${data.contributorLink}
+
+  ## GitHub link
+  ${data.link}
+  ### &copy; ${new Date().getFullYear()} by ${data.name}
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = data =>{
+  console.log(data)
+  return`
+    ${generateMarkdown(data)}
+    `
+
+};
---
+++ index.js
@@ -0,0 +1,136 @@
+
+const inquirer = require("inquirer");
+
+const fs = require('fs');
+
+const generateMarkdown = require('./Develop/utils/generateMarkdown');
+
+const promptProject = portfolioData => {
+
+    // if (!portfolioData.projects) {
+    //     portfolioData.projects = [];
+    // }
+    return inquirer.prompt([
+      {
+          type: "input", 
+          name: "title",
+          message: "what is the title of this project?",
+          validate : nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter Project title!");
+                return false;
+            }
+        }
+        }, 
+        {
+            type: "input", 
+            name: "name",
+            message: "what is the name of this developer?",
+            validate : nameInput => {
+              if (nameInput) {
+                  return true;
+              } else {
+                  console.log("Please enter developer's name!");
+                  return false;
+              }
+          }
+          }, 
+        {
+          type: "input",
+          name: "description",
+          message: "Provide a description for the project: "
+        }, 
+        {
+            type: "input",
+            name: "installation",
+            message: "What is required to install your project? "
+        },  
+        {
+            type: "input",
+            name: "demo",
+            message: "Image or video to display the functions of your app "
+        },  
+      {
+          type: "checkbox", 
+          name: "languages", 
+          message: "What did you build this project with? (Check all that apply) ", 
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node', "MySOL"]
+      }, 
+      {
+          type: "input",
+          name: "link", 
+          message: "enter the GitHub link to our project: (Required)", 
+          validate : nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter GitHub link!");
+                return false;
+            }
+        }
+      }, 
+        {
+          type: "confirm", 
+          name: "license", 
+          message: "Would you like to apply a MIT License to your project? ",
+          default: true
+        }, 
+        {
+            type: "confirm", 
+            name: "confirmContributors", 
+            message: "Anyone contributed to this project besides yourself? ",
+            default: false
+        }, 
+        {
+          type: "input",
+          name: "contributorName", 
+          message: "Who contributed to your project?",
+          when: ({confirmContributors}) => {
+            if (confirmContributors) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        },
+        {
+        type: "input",
+        name: "contributorLink", 
+        message: "Contributor's GitHub: ",
+        when: ({confirmContributors}) => {
+          if (confirmContributors) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+        },
+
+
+
+    ])
+    .then(projectData => {
+        console.log(projectData)
+            return projectData
+    })
+  };
+  promptProject()
+  .then(projectData => {
+    const pageMD = generateMarkdown(projectData);
+    fs.writeFile('./dist/README.md' , pageMD, err => {
+        if (err) throw new Error(err);
+        console.log("Page created!")
+        if (projectData.license){
+            fs.copyFile("./develop/utils/license.txt", "./dist/license.txt", err=> {
+                if(err){
+                    console.log(err);
+                    return;
+                }
+                console.log("license added");
+            })
+        }
+    });
+  });
+
